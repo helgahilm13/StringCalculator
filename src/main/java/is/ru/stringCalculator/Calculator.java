c2ab532dbff2f91ee75095624fc473319d2b8fdb
helga13 <helga13@ru.is>
2017-10-15 21:02:01 +0000

    Getting started

---
+++ src/main/java/is/ru/stringCalculator/Calculator.java
@@ -0,0 +1,29 @@
+package is.ru.stringCalculator;
+
+public class Calculator {
+	
+	public static int add(String text) {
+
+		if(text.equals(""))
+			return 0;
+		else{
+			if(text.contains(",")){
+				String numbers[] = text.split(",");
+				return sum(numbers);
+			}
+			return 1;
+		}
+	}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	private static int sum(){
+		int total = 0;
+		for(String number : numbers){
+			total += toInt(number);
+		}
+		return total;
+	}
+}
